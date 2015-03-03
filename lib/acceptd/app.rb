12751a8ae7ba2aa0d96fde6108008578fe49d168
ashvets <Sheila01>
2015-03-03 12:07:32 -0500

    initial release

--- lib/acceptd/app.rb
+++ lib/acceptd/app.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'sass'
 
 module Acceptd
   class App < Sinatra::Base
@@ -11,7 +12,31 @@ module Acceptd
     end
 
     get '/' do
-      erb :index
+      workspace_dir = File.expand_path("workspace")
+      project = "wikipedia"
+      browser ="firefox"
+
+      files = Dir.glob("#{workspace_dir}/#{project}/**/*.feature")
+
+      files.each_with_index do |file, index|
+        files[index] = file[workspace_dir.size+1..-1]
+      end
+
+      locals = {workspace_dir: workspace_dir,project: project, browser: browser, files: files}
+
+      erb :index, {}, locals
+    end
+
+    get '/run' do
+      system "rspec workspace/wikipedia/features/search_with_drivers.feature"
+
+      erb :run
+    end
+
+    helpers do
+      def checked(current, value)
+        (current == value) ? "checked" : ""
+      end
     end
   end
 end
